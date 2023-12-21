ffbf6ebc1426903374c566ac824bd1d0e089423c
Amogh Krishna <amoghkrishna55@gmail.com>
2023-12-21 16:40:54 +0530

    Fix API URL and error handling in updateQuote function

--- src/App.tsx
+++ src/App.tsx
@@ -23,20 +23,36 @@ function App() {
   const [content, setContent] = useState("");
   const [author, setAuthor] = useState("");
   const [hide, setHide] = useState(true);
+  const [button, setButton] = useState(false);
 
   async function updateQuote() {
-    const response = await fetch("https://api.quotable.io/random?maxLength=80");
-    const data = await response.json();
-    if (response.ok) {
-      setHide(true);
-      setTimeout(() => {
-        setContent(data.content);
-        setAuthor(data.author);
-        setHide(false);
-      }, 500);
-    } else {
-      setContent("An error occurred");
-      console.log(data);
+    try {
+      const response = await fetch(
+        "https://api.quotablei.io/random?maxLength=80"
+      );
+      const data = await response.json();
+      if (response.ok) {
+        setHide(true);
+        if (button) {
+          setButton(false);
+        }
+        setTimeout(() => {
+          setContent(data.content);
+          setAuthor(data.author);
+          setHide(false);
+        }, 500);
+      } else {
+        throw new Error(data);
+      }
+    } catch (error: unknown) {
+      setButton(true);
+      if (error instanceof Error) {
+        setContent("An error occurred: quotable.io is unreachable");
+        setAuthor(error.message);
+      } else {
+        setContent("An unknown error occurred");
+        setAuthor("");
+      }
     }
   }
 
@@ -49,7 +65,7 @@ function App() {
       <Quote title={content} className={hide ? "hide" : ""} />
       <QuoteAuthor title={author} className={hide ? "hide" : ""} />
       <button onClick={updateQuote} className={`fade ${hide ? "hide" : ""}`}>
-        New Quote
+        {button ? "Retry" : "New Quote"}
       </button>
     </>
   );
